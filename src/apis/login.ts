d0624d06e14937905643ab5ba79682519c612dc7
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-09-12 11:19:20 +0900

    refactor : 관리자 페이지 url 변경

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -1,6 +1,6 @@
 import request from './core'
 
 export const login = async <T>(data: LoginRequest): Promise<T> => {
-  const url = '/auth/logIn'
+  const url = '/admin/auth/logIn'
   return await request.post<T>(url, data)
 }
